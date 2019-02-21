1c8d853cd8f0819ec66f6ef59293e8fc72df3815
Hon Loon <tenghonloon@hotmail.com>
2019-02-21 23:10:58 +0800

    Dependency Inversion Principle

---
+++ DependencyInversionPrinciple/Program.cs
@@ -0,0 +1,74 @@
+﻿namespace DependencyInversionPrinciple
+{
+    class Program
+    {
+        /*
+         * Intent
+         * High-level modules should not depend on low-level modules. Both should depend on abstractions.
+         * Abstractions should not depend on details. Details should depend on abstractions.
+         * https://stackoverflow.com/questions/62539/what-is-the-dependency-inversion-principle-and-why-is-it-important
+         */
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    // Dependency Inversion Principle - Bad example
+    //public class Worker
+    //{
+    //    public void Work()
+    //    {
+    //        //working...
+    //    }
+    //}
+
+    //public class SuperWorker
+    //{
+    //    public void Work()
+    //    {
+    //        //working...
+    //    }
+    //}
+
+    //public class Manager
+    //{
+    //    public Worker Worker { private get; set; }
+
+    //    public void Manage()
+    //    {
+    //        Worker.Work();
+    //    }
+    //}
+
+    // Dependency Inversion Principle - Good example
+    public interface IWorker
+    {
+        void Work();
+    }
+
+    public class Worker : IWorker
+    {
+        public void Work()
+        {
+            // ....working
+        }
+    }
+
+    public class SuperWorker : IWorker
+    {
+        public void Work()
+        {
+            //.... working much more
+        }
+    }
+
+    public class Manager
+    {
+        public IWorker Worker { private get; set; }
+
+        public void Manage()
+        {
+            Worker.Work();
+        }
+    }
+}
---
+++ OpenClosePrinciple/Program.cs
@@ -0,0 +1,90 @@
+﻿namespace OpenClosePrinciple
+{
+    class Program
+    {
+        /*
+         * Intent
+         * Software entities like classes, modules and functions should be open for extension but closed for modifications.
+         */
+        static void Main(string[] args)
+        {
+        }
+    }
+    // Open-Close Principle - Good example
+    public class GraphicEditor
+    {
+        public void DrawShape(Shape shape)
+        {
+            shape.Draw();
+        }
+    }
+
+    public abstract class Shape
+    {
+        public abstract void Draw();
+    }
+
+    public class Rectangle : Shape
+    {
+        public override void Draw()
+        {
+            // draw the rectangle
+        }
+    }
+
+    public class Circle : Shape
+    {
+        public override void Draw()
+        {
+            // draw the circle
+        }
+    }
+
+    // Open-Close Principle - Bad example
+    //public class GraphicEditor
+    //{
+    //    public void DrawShape(Shape shape)
+    //    {
+    //        switch (shape.Type)
+    //        {
+    //            case 1:
+    //                DrawRectangle(shape as Rectangle);
+    //                break;
+    //            case 2:
+    //                DrawCircle(shape as Circle);
+    //                break;
+    //        }
+    //    }
+
+    //    public void DrawCircle(Circle circle)
+    //    {
+    //        //...
+    //    }
+
+    //    public void DrawRectangle(Rectangle rectangle)
+    //    {
+    //        //...
+    //    }
+    //}
+
+    //public class Shape
+    //{
+    //    public int Type { get; set; }
+    //}
+
+    //public class Rectangle : Shape
+    //{
+    //    public Rectangle()
+    //    {
+    //        Type = 1;
+    //    }
+    //}
+
+    //public class Circle : Shape
+    //{
+    //    public Circle()
+    //    {
+    //        Type = 2;
+    //    }
+    //}
+}
